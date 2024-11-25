4e664b73ebcb4a77d8efd20afbf2e3851a392152
Nicolas Assis <145511703+Roludojr@users.noreply.github.com>
2024-11-25 09:34:31 -0300

    arquiivos de comit

---
+++ Cabeçalho.java
@@ -0,0 +1,24 @@
+public class Cabeçalho {
+    String nome_da_faculdade = "";
+    String nome_do_aluno = "";
+    String nome_do_professor = "";
+    String menssagens_de_boas_vindas = "";
+
+    public  Cabeçalho(){
+
+
+
+
+    }
+
+
+    public String toString(){
+        return "" + nome_da_faculdade;
+
+
+    }
+
+
+    }
+
+

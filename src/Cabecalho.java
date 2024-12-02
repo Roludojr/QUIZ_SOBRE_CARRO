e2ad6d6f6f8d5bf98ff42d1b833ae44fb54141ce
Nicolas Assis <mandapronicolas@gmail.com>
2024-12-02 14:42:56 -0300

    Colocando as ultimas 15 questões adicionando o contador de acerto e erros

--- src/Cabecalho.java
+++ src/Cabecalho.java
@@ -1,7 +1,7 @@
 public class Cabecalho {
 
     public static void escrava_na_tela (){
-        System.out.println("Unifam");
+        System.out.println("Unifam -------------- 11/2024\n Professor: Brenno Pimenta \n  Aluno: Nicolas Assis F. Jesus \n   Bem vindo ao quiz sobre CARROS!\n    Vamos começar!"  );
     }
 
 
--- src/QUIZ.java
+++ src/QUIZ.java
@@ -2,18 +2,15 @@ public class QUIZ {
     public static void main(String[] args) {
 
         Cabecalho.escrava_na_tela();
-        //Cabecalho c = new Cabecalho();
-       // c.escrava_na_tela();
 
-        int contador = 0;
+        double contador = 0;
         double taxa_de_acerto;
 
-        System.out.println("Bem-vindo ao quiz!");
 
         Questao q1 = new Questao();
 
         // Configurando os atributos da questão
-        q1.pergunta = "Qual é o nome do equipamento que reduz as emissões de gases poluentes nos carros?";
+        q1.pergunta = " 1): Qual é o nome do equipamento que reduz as emissões de gases poluentes nos carros?";
         q1.opcaoA = "A) Catalisador";
         q1.opcaoB = "B) Radiador";
         q1.opcaoC = "C) Alternador";
@@ -33,10 +30,12 @@ public class QUIZ {
         if (q1.isCorreta(respostaUsuario))
             contador = contador+ 1;
 
+        System.out.println("------------------------------------------------------------------------");
+
 
         //questoes da segunda perguta do quiz
         Questao q2 = new Questao();
-        q2.pergunta= "Qual componente é responsável por transmitir a potência do motor para as rodas?";
+        q2.pergunta= "2): Qual componente é responsável por transmitir a potência do motor para as rodas?";
         q2.opcaoA = "A) Transmissão";
         q2.opcaoB = "B) Radiador";
         q2.opcaoC = "C) Alternador";
@@ -53,9 +52,10 @@ public class QUIZ {
         if (q2.isCorreta(respostaUsuario2))
             contador = contador+ 1;
 
+        System.out.println("------------------------------------------------------------------------");
 
         Questao q3 = new Questao();
-        q3.pergunta = "Qual é o nome do sistema que impede que as rodas do carro patinem ao arrancar?";
+        q3.pergunta = "3): Qual é o nome do sistema que impede que as rodas do carro patinem ao arrancar?";
         q3.opcaoA = "A) ABS";
         q3.opcaoB = "B) Controle de tração";
         q3.opcaoC = "C) Amortecedor";
@@ -73,9 +73,10 @@ public class QUIZ {
             contador = contador + 1;
         taxa_de_acerto = (contador * 100) /15;
 
+        System.out.println("------------------------------------------------------------------------");
 
         Questao q4 = new Questao();
-        q4.pergunta = "Qual é a principal função do diferencial em um veículo?";
+        q4.pergunta = "4): Qual é a principal função do diferencial em um veículo?";
         q4.opcaoA = "A) Distribuir a força igualmente entre as rodas.";
         q4.opcaoB = "B) Reduzir o desgaste dos pneus.";
         q4.opcaoC = "C) Aumentar a potência do motor.";
@@ -93,8 +94,11 @@ public class QUIZ {
             contador = contador + 1;
         taxa_de_acerto = (contador * 100) /15;
 
+        System.out.println("------------------------------------------------------------------------");
+
+
         Questao q5 = new Questao();
-        q5.pergunta = "Quem é considerado o inventor do primeiro automóvel movido a gasolina?";
+        q5.pergunta = "5): Quem é considerado o inventor do primeiro automóvel movido a gasolina?";
         q5.opcaoA = "A) Henry Ford";
         q5.opcaoB = "B) Karl Benz";
         q5.opcaoC = "C) Nikolaus Otto";
@@ -112,8 +116,10 @@ public class QUIZ {
             contador = contador + 1;
         taxa_de_acerto = (contador * 100) /15;
 
+        System.out.println("------------------------------------------------------------------------");
+
         Questao q6 = new Questao();
-        q6.pergunta = "Em que ano foi lançado o Ford Model T, o primeiro carro produzido em massa?";
+        q6.pergunta = "6): Em que ano foi lançado o Ford Model T, o primeiro carro produzido em massa?";
         q6.opcaoA = "A) 1915";
         q6.opcaoB = "B) 1920";
         q6.opcaoC = "C) 1895";
@@ -131,9 +137,10 @@ public class QUIZ {
             contador = contador + 1;
         taxa_de_acerto = (contador * 100) /15;
 
+        System.out.println("------------------------------------------------------------------------");
 
         Questao q7 = new Questao();
-        q7.pergunta = "Qual foi o primeiro carro esportivo da Ferrari?";
+        q7.pergunta = "7): Qual foi o primeiro carro esportivo da Ferrari?";
         q7.opcaoA = "A) Ferrari 250 GTO";
         q7.opcaoB = "B) Ferrari Dino";
         q7.opcaoC = "C) Ferrari Testarossa";
@@ -151,9 +158,183 @@ public class QUIZ {
             contador = contador + 1;
         taxa_de_acerto = (contador * 100) /15;
 
+        System.out.println("------------------------------------------------------------------------");
 
-       // System.out.println("parebéns você ja tem um acerto de " + contador);
-        // System.out.println(" taxa de acerto em "+taxa_de_acerto+"%");
+        Questao q8 = new Questao();
+        q8.pergunta = "8): Qual dessas marcas pertence ao grupo Volkswagen?";
+        q8.opcaoA = "A) Ferrari";
+        q8.opcaoB = "B) Bentley";
+        q8.opcaoC = "C) Tesla";
+        q8.opcaoD = "D) Rolls-Royce";
+        q8.opcaoE = "E) Lexus";
+        q8.correta = "B";
+
+        q8.escrevaQuestao();
+        // lendo a resposta do usuario
+        String respostaUsuario8 = q8.leiaResposta();
+
+
+        q8.isCorreta(respostaUsuario8);
+        if (q8.isCorreta(respostaUsuario8))
+            contador = contador + 1;
+        taxa_de_acerto = (contador * 100) /15;
+
+
+        System.out.println("------------------------------------------------------------------------");
+
+        Questao q9 = new Questao();
+        q9.pergunta = "9): Qual é o modelo de carro esportivo mais vendido de todos os tempos?";
+        q9.opcaoA = "A) Ford Mustang";
+        q9.opcaoB = "B) Chevrolet Corvette";
+        q9.opcaoC = "C) Porsche 911";
+        q9.opcaoD = "D) Mazda MX-5 Miata";
+        q9.opcaoE = "E) Dodge Challenger";
+        q9.correta = "D";
+
+        q9.escrevaQuestao();
+        // lendo a resposta do usuario
+        String respostaUsuario9 = q9.leiaResposta();
+
+
+        q9.isCorreta(respostaUsuario);
+        if (q9.isCorreta(respostaUsuario9))
+            contador = contador + 1;
+        taxa_de_acerto = (contador * 100) /15;
+
+        System.out.println("------------------------------------------------------------------------");
+
+        Questao q10 = new Questao();
+        q10.pergunta = "10): Qual montadora fabrica o famoso modelo Hilux?";
+        q10.opcaoA = "A) Mitsubishi";
+        q10.opcaoB = "B) Chevrolet";
+        q10.opcaoC = "C) Ford";
+        q10.opcaoD = "D) Toyota";
+        q10.opcaoE = "E) Nissan";
+        q10.correta = "D";
+
+        q10.escrevaQuestao();
+        // lendo a resposta do usuario
+        String respostaUsuario10 = q10.leiaResposta();
+
+
+        q10.isCorreta(respostaUsuario10);
+        if (q10.isCorreta(respostaUsuario10))
+            contador = contador + 1;
+        taxa_de_acerto = (contador * 100) /15;
+
+        System.out.println("------------------------------------------------------------------------");
+
+        Questao q11 = new Questao();
+        q11.pergunta = "11): Qual é a principal função do turbocharger em um motor?";
+        q11.opcaoA = "A) Reduzir o consumo de combustível";
+        q11.opcaoB = "B) Aumentar a potência ao comprimir mais ar para o motor";
+        q11.opcaoC = "C) Diminuir a emissão de poluentes";
+        q11.opcaoD = "D) Melhorar a refrigeração do motor";
+        q11.opcaoE = "E)  Reduzir o ruído do motor";
+        q11.correta = "B";
+
+        q11.escrevaQuestao();
+        // lendo a resposta do usuario
+        String respostaUsuario11 = q11.leiaResposta();
+
+
+        q11.isCorreta(respostaUsuario11);
+        if (q11.isCorreta(respostaUsuario11))
+            contador = contador + 1;
+        taxa_de_acerto = (contador * 100) /15;
+
+        System.out.println("------------------------------------------------------------------------");
+
+        Questao q12 = new Questao();
+        q12.pergunta = "12): Qual foi o primeiro carro de Fórmula 1 a usar motor híbrido com sucesso?";
+        q12.opcaoA = "A) McLaren MP4/4";
+        q12.opcaoB = "B) Red Bull RB9";
+        q12.opcaoC = "C) Mercedes F1 W05 Hybrid";
+        q12.opcaoD = "D) Ferrari SF90";
+        q12.opcaoE = "E) Renault RS20";
+        q12.correta = "C";
+
+        q12.escrevaQuestao();
+        // lendo a resposta do usuario
+        String respostaUsuario12 = q12.leiaResposta();
+
+
+        q12.isCorreta(respostaUsuario12);
+        if (q12.isCorreta(respostaUsuario12))
+            contador = contador + 1;
+        taxa_de_acerto = (contador * 100) /15;
+
+        System.out.println("------------------------------------------------------------------------");
+
+        Questao q13 = new Questao();
+        q13.pergunta = "13): Qual é a marca com mais títulos na história da Fórmula 1?";
+        q13.opcaoA = "A) Ferrari";
+        q13.opcaoB = "B) McLaren";
+        q13.opcaoC = "C) Mercedes";
+        q13.opcaoD = "D) Williams";
+        q13.opcaoE = "E) Red Bull";
+        q13.correta = "A";
+
+        q13.escrevaQuestao();
+        // lendo a resposta do usuario
+        String respostaUsuario13 = q13.leiaResposta();
+
+
+        q13.isCorreta(respostaUsuario13);
+        if (q13.isCorreta(respostaUsuario13))
+            contador = contador + 1;
+        taxa_de_acerto = (contador * 100) /15;
+
+        System.out.println("------------------------------------------------------------------------");
+
+        Questao q14 = new Questao();
+        q14.pergunta = "14): Qual carro ficou famoso como Herbie nos filmes??";
+        q14.opcaoA = "A) Mini Cooper";
+        q14.opcaoB = "B) Volkswagen Fusca";
+        q14.opcaoC = "C) Ford Mustang";
+        q14.opcaoD = "D) Chevrolet Camaro";
+        q14.opcaoE = "E) Dodge Charger";
+        q14.correta = "B";
+
+        q14.escrevaQuestao();
+        // lendo a resposta do usuario
+        String respostaUsuario14 = q14.leiaResposta();
+
+
+        q14.isCorreta(respostaUsuario14);
+        if (q14.isCorreta(respostaUsuario14))
+            contador = contador + 1;
+        taxa_de_acerto = (contador * 100) /15;
+
+        System.out.println("------------------------------------------------------------------------");
+
+        Questao q15 = new Questao();
+        q15.pergunta = "15): Em que país foi fundada a montadora Volvo?";
+        q15.opcaoA = "A) Alemanha";
+        q15.opcaoB = "B) Suíça";
+        q15.opcaoC = "C) Suécia";
+        q15.opcaoD = "D) Noruega";
+        q15.opcaoE = "E) Finlândia";
+        q15.correta = "C";
+
+        q15.escrevaQuestao();
+        // lendo a resposta do usuario
+        String respostaUsuario15 = q15.leiaResposta();
+
+
+        q15.isCorreta(respostaUsuario15);
+        if (q15.isCorreta(respostaUsuario15))
+            contador = contador + 1;
+        taxa_de_acerto = (contador * 100) /15;
+
+
+        System.out.println("------------------------------------------------------------------------");
+
+        double taxa_de_erro = (15 - contador);
+        System.out.println("Parebéns você acertou: " + contador + " questões");
+        System.out.println("Questões erradas: " + taxa_de_erro + " questões");
+        System.out.printf("Sua taxa de acerto foi de: %.2f%%\n", taxa_de_acerto);
+
 
     }
 }
